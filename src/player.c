01be4926bd5db2dc51ba258d0393d77d27a04e65
Apmds <ap1mrfj@gmail.com>
2025-04-12 11:34:28 +0100

    fix fisheye

--- src/player.c
+++ src/player.c
@@ -167,10 +167,15 @@ void PlayerDraw3D(Player p, int screenWidth, int screenHeight) {
         int rayX = (line_width/2)+i*line_width;
 
         Vector2 collisionPoint = MapRayGetCollisionPoint(ray);
+        // Distance calculated with fisheye
         //double distance = (double) MapRayGetMaxLength(ray) / Vector2Distance(collisionPoint, (Vector2) {p->posX, p->posY});
         //distance *= cos(-((p->FOV/2))*DEG2RAD);
         //distance *= 50;
-        double distance = 50*screenHeight / MapRayGetLength(ray);
+        
+        double distance = (50*screenHeight) / (MapRayGetLength(ray)*cos(MapRayGetAngleOffsetRad(ray)));
+        if (distance > screenHeight) {
+            distance = screenHeight;
+        }
 
         Color drawColor;
         if (MapRayGetHitSide(ray) == X_AXIS) {
